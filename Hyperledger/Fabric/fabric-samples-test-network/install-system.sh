d01332df5bd206547cc8c203271ae9db5e283e94
Kurt Seifried <kurt@seifried.org>
2021-03-17 09:28:49 -0600

    creating the channel explicitly seems to work better

--- Hyperledger/Fabric/fabric-samples-test-network/install-system.sh
+++ Hyperledger/Fabric/fabric-samples-test-network/install-system.sh
@@ -68,8 +68,7 @@ cat <<'EOF' >> /opt/hyperledger/3-node-chain-start.sh
 #
 cd /opt/hyperledger/fabric-samples/test-network
 # defaults to "mychannel", this is mandatory as the addOrg3 scripts expects mychannel to exist
-./network.sh up
-./network.sh createChannel -c mychannel
+./network.sh up createChannel -c mychannel
 #
 cd /opt/hyperledger/fabric-samples/test-network/addOrg3/
 ./addOrg3.sh up -c mychannel
