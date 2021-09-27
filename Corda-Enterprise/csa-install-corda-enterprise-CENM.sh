174afec1a77fdebfd88989c2c4ae80c7da3e53a0
Kurt Seifried <kurt@seifried.org>
2021-09-27 11:53:29 -0700

    ipv6

--- Corda-Enterprise/csa-install-corda-enterprise-CENM.sh
+++ Corda-Enterprise/csa-install-corda-enterprise-CENM.sh
@@ -27,6 +27,8 @@ fi
 #/etc/sysctl.conf
 #net.ipv6.conf.all.disable_ipv6=1
 #net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
 #
 # Check for free disk space
